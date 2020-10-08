5635ff7fe511a8d72bb46dfccd87be826c887677
Swaraj1120 <swarajofficial94@gmail.com>
2020-10-08 20:57:03 +0530

    demo

--- app/src/main/java/com/swaraj/eggstopwatch/MainActivity.java
+++ app/src/main/java/com/swaraj/eggstopwatch/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MinActivity extends AppCompatActivity {
     CountDownTimer countDownTimer;
     MediaPlayer mPlayer;
     SeekBar timerController;
